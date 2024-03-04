3e637dd2f150c9eab6527ab0c638a83f48d033ab
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-05 01:33:29 +0300

    Getting object keys

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+    const report = {
+      allEmployees: employeesList,
+      getNumberOfDepartments(employees) {
+        return Object.keys(employees).length;
+      },
+    };
+    return report;
+}
