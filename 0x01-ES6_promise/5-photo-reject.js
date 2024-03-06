1cea5eb2062cfc0c49601ea8a72066c3cd7230dd
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-07 00:49:15 +0300

    Add resolve

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => {
+  return new Promise((resolve, reject) => {
     reject(new Error(`${fileName} cannot be processed`));
   });
 }
