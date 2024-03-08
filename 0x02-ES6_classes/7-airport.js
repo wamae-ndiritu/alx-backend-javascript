e58a2bfcf92d0589459fbff16187ee9933d2e8b2
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-08 03:21:25 +0300

    Create class with method that overides the deafult string method

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,21 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Override toString method
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
