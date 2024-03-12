66ef79d65ddef90ac7110e187ead9d51f321e9b4
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-13 00:03:01 +0300

    Fix some issue in cleanSet

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 function cleanSet(setItems, startsWith) {
   let restStr = '';
   for (const item of setItems) {
-    if (startsWith !== '' && item.startsWith(startsWith)) {
-      const lastPart = item.split(startsWith)[1];
-      restStr += `${lastPart}-`;
+    if (item.startsWith(startsWith)) {
+      restStr
+        += (restStr ? '-' : '')
+        + (startsWith ? item.slice(startsWith.length) : '');
     }
   }
-  return restStr.slice(0, -1);
+  return restStr;
 }
 
 export default cleanSet;
